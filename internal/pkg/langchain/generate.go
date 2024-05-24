5165e5d0ca3788baa3c6d4b755eb32308e380f5a
William Cook <web@casadecook.com>
2024-05-23 22:52:05 -0400

    Tune grounding prompt

--- internal/pkg/langchain/generate.go
+++ internal/pkg/langchain/generate.go
@@ -9,18 +9,17 @@ import (
 	"github.com/wgeorgecook/plex-recommendation/internal/pkg/plex"
 )
 
-func GenerateRecommendation(ctx context.Context, videos []plex.VideoShort, llm *llms.Model) (string, error) {
+func GenerateRecommendation(ctx context.Context, recentlyViewed, fullCollection []plex.VideoShort, llm *llms.Model) (string, error) {
 	log.Println("generating recommendation...")
-	grounding := `Given the following videos from my watch history, 
-	recommend me one or two videos to watch next. Ensure that the rating on your 
-	recommendations do not exceed that of my latest viewings 
-	(EG, if I have recently watching only G and PG rated films, do not 
-	recommend a PG-13 rated film to me.). Provide me the title, summary, 
-	and letter rating of the film in your recommentation. 
-	
-	%+v\n`
+	grounding := `Please recommend me up to five different movies to watch based on my recent watch
+	history provided here: %+v. Please do not suggest any titles that do not exist in the following 
+	collection: %+v. Do not recommend me any titles that have a content rating exceeding the highest
+	content rating in my recent watch history. I have a child in the family, so take into consideration
+	something a toddler would also enjoy watching. Provide your recommendations in the following format:
+	Title (ContentRating): Summary
+	`
 
-	recommendation, err := llms.GenerateFromSinglePrompt(ctx, *llm, fmt.Sprintf(grounding, videos))
+	recommendation, err := llms.GenerateFromSinglePrompt(ctx, *llm, fmt.Sprintf(grounding, recentlyViewed, fullCollection))
 	if err != nil {
 		return "", err
 	}
