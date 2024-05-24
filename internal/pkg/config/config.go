8a1cbbe76e7e7e11b4d5a54d4327667950456667
William Cook <web@casadecook.com>
2024-05-23 22:20:56 -0400

    Get recently watched limit

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -3,13 +3,21 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	PlexToken   string
-	PlexAddress string
+	Plex struct {
+		Token   string
+		Address string
+	}
+	Ollama struct {
+		Address string
+		Model   string
+	}
+	RecentMovieCount int
 }
 
 // loadEnv loads environment variables from a .env file.
@@ -25,11 +33,24 @@ func LoadConfig() *Config {
 
 	var cfg Config
 	if os.Getenv("PLEX_TOKEN") != "" {
-		cfg.PlexToken = os.Getenv("PLEX_TOKEN")
+		cfg.Plex.Token = os.Getenv("PLEX_TOKEN")
 	}
 	if os.Getenv("PLEX_ADDRESS") != "" {
-		cfg.PlexAddress = os.Getenv("PLEX_ADDRESS")
+		cfg.Plex.Address = os.Getenv("PLEX_ADDRESS")
+	}
+	if os.Getenv("OLLAMA_ADDRESS") != "" {
+		cfg.Ollama.Address = os.Getenv("OLLAMA_ADDRESS")
+	}
+	if os.Getenv("OLLAMA_MODEL") != "" {
+		cfg.Ollama.Model = os.Getenv("OLLAMA_MODEL")
 	}
 
+	recentMovieCountStr := os.Getenv("RECENT_MOVIE_COUNT")
+	count, err := strconv.Atoi(recentMovieCountStr)
+	if recentMovieCountStr == "" || err != nil {
+		cfg.RecentMovieCount = 5
+	} else {
+		cfg.RecentMovieCount = count
+	}
 	return &cfg
 }
