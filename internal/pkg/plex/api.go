106d1401da8e8d4be9cb58c7b592b8a756866060
William Cook <web@casadecook.com>
2024-05-23 22:21:19 -0400

    Logging

--- internal/pkg/plex/api.go
+++ internal/pkg/plex/api.go
@@ -32,10 +32,18 @@ type Video struct {
 	Summary       string   `xml:"summary,attr"`
 }
 
-func GetRecentlyPlayed(c Client, sectionId string) ([]Video, error) {
-	connectionUri := c.Connect(sectionId)
-	log.Println("connectionUri: " + connectionUri)
+type VideoShort struct {
+	Title         string
+	Summary       string
+	ContentRating string
+}
 
+func GetRecentlyPlayed(c Client, sectionId string) ([]Video, error) {
+	log.Println("connecting to Plex...")
+	connectionUri := c.Connect(sectionId)
+	log.Println("connected")
+
+	log.Println("getting recently watched...")
 	resp, err := c.MakeNetworkRequest(connectionUri, http.MethodGet)
 	if err != nil {
 		return nil, err
@@ -46,12 +54,19 @@ func GetRecentlyPlayed(c Client, sectionId string) ([]Video, error) {
 	if err != nil {
 		return nil, err
 	}
+	log.Println("received")
 
 	var container MediaContainer
 	if err := xml.Unmarshal(bodyBytes, &container); err != nil {
 		return nil, err
 	}
+	log.Printf("total count: %v\n", len(container.Videos))
+	log.Println("returning recently watched")
 
 	return container.Videos, nil
 
+}
+
+func GetAllVideos(c Client, sectionId string) ([]VideoShort, error) {
+	return nil, nil
 }
