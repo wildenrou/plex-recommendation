c33009a1c862aa2174f78c82c88a1c1a3effdcb3
William Cook <web@casadecook.com>
2024-05-23 22:19:59 -0400

    Generate recommendation from recently watched

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/wgeorgecook/plex-recommendation/internal/pkg/config"
+	"github.com/wgeorgecook/plex-recommendation/internal/pkg/langchain"
 	"github.com/wgeorgecook/plex-recommendation/internal/pkg/plex"
 )
 
@@ -13,14 +16,43 @@ func main() {
 	defer fmt.Println("good bye 👋")
 
 	cfg := config.LoadConfig()
-
-	c := plex.New(cfg.PlexToken, cfg.PlexAddress)
+	log.Printf("config loaded: %+v\n", cfg)
+	c := plex.New(cfg.Plex.Token, cfg.Plex.Address)
 	vids, err := plex.GetRecentlyPlayed(c, "3")
 	if err != nil {
 		panic(err)
 	}
 
-	for _, video := range vids {
-		log.Printf("title: %s\ndescription: %s\nrating: %s\n", video.Title, video.Summary, video.ContentRating)
+	recentViews := make([]plex.VideoShort, 0, cfg.RecentMovieCount)
+	log.Printf("making shortvid list of length %v\n", cfg.RecentMovieCount)
+	for i, vid := range vids {
+		if i > cfg.RecentMovieCount {
+			break
+		}
+
+		recentViews = append(recentViews, plex.VideoShort{Title: vid.Title, Summary: vid.Summary, ContentRating: vid.ContentRating})
 	}
+
+	log.Printf("Recent views: %+v\n", recentViews)
+
+	ollama, err := langchain.InitOllama(cfg.Ollama.Address, cfg.Ollama.Model)
+	if err != nil {
+		panic(err)
+	}
+
+	runSimple := os.Getenv("RUN_SIMPLE")
+	full := runSimple == ""
+	var recommendation string
+	if full {
+
+		recommendation, err = langchain.GenerateRecommendation(context.Background(), recentViews, &ollama)
+
+	} else {
+		recommendation, err = langchain.GenerateSimpleRecommendation(context.Background(), &ollama)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("recommendation: %s\n", recommendation)
 }
