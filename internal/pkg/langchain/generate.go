737f146e77bc4808d13a1412682ebb936a67e349
William Cook <web@casadecook.com>
2024-06-10 22:48:27 -0400

    Tweak grounding for more limited and structured output

--- internal/pkg/langchain/generate.go
+++ internal/pkg/langchain/generate.go
@@ -6,19 +6,27 @@ import (
 	"log"
 
 	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
 )
 
-func GenerateRecommendation(ctx context.Context, recentlyViewed, fullCollection string, llm *llms.Model) (string, error) {
+func GenerateRecommendation(ctx context.Context, recentlyViewed, fullCollection string, llm *ollama.LLM) (string, error) {
 	log.Println("generating recommendation...")
-	grounding := `Please recommend me up to five different movies to watch based on my recent watch
+	grounding := `Please recommend me up to 3 different movies to watch based on my recent watch
 	history provided here: %+v. Please do not suggest any titles that do not exist in the following 
 	collection: %+v. Do not recommend me any titles that have a content rating exceeding the highest
 	content rating in my recent watch history. I have a child in the family, so take into consideration
-	something a toddler would also enjoy watching. Provide your recommendations in the following format:
-	Title (ContentRating): Summary
+	something a toddler would also enjoy watching. Please provide your recommendation as a json array of
+	objects, whose members have this shape:
+	{
+		"title": title,
+		"summary": summary,
+		"content_rating": content_rating
+	}
+	Please do not recomment more than 3 titles. Please do ensure your response is valid json before
+	returning it to me.
 	`
 
-	recommendation, err := llms.GenerateFromSinglePrompt(ctx, *llm, fmt.Sprintf(grounding, recentlyViewed, fullCollection))
+	recommendation, err := llms.GenerateFromSinglePrompt(ctx, llm, fmt.Sprintf(grounding, recentlyViewed, fullCollection))
 	if err != nil {
 		return "", err
 	}
@@ -28,11 +36,11 @@ func GenerateRecommendation(ctx context.Context, recentlyViewed, fullCollection 
 
 }
 
-func GenerateSimpleRecommendation(ctx context.Context, llm *llms.Model) (string, error) {
+func GenerateSimpleRecommendation(ctx context.Context, llm *ollama.LLM) (string, error) {
 	log.Println("generating recommendation...")
 	grounding := "Hello! How are you today?"
 
-	recommendation, err := llms.GenerateFromSinglePrompt(ctx, *llm, grounding)
+	recommendation, err := llms.GenerateFromSinglePrompt(ctx, llm, grounding)
 	if err != nil {
 		return "", err
 	}
