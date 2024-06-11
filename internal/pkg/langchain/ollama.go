141d5542535861c21fe1fc76bd06f29ca1cc65c2
William Cook <web@casadecook.com>
2024-06-10 22:48:10 -0400

    Init separate embedding and language model clients

--- internal/pkg/langchain/ollama.go
+++ internal/pkg/langchain/ollama.go
@@ -3,25 +3,32 @@ package langchain
 import (
 	"log"
 
-	"github.com/tmc/langchaingo/embeddings"
-	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/ollama"
 )
 
 // InitOllama is the entrypoint for interacting with Ollama provided
 // LLMs
-func InitOllama(address, model string) (llms.Model, *embeddings.EmbedderImpl, error) {
+func InitOllama(address, languageModel, embeddingModel string) (*ollama.LLM, *ollama.LLM, error) {
 
 	log.Println("initializing Ollama...")
+
+	log.Println("initializing LLM...")
 	var serverurl = "http://" + address + ":11434"
-	llm, err := ollama.New(ollama.WithModel(model), ollama.WithServerURL(serverurl))
+	llm, err := ollama.New(ollama.WithModel(languageModel), ollama.WithServerURL(serverurl))
 	if err != nil {
 		return nil, nil, err
 	}
-	embedder, err := embeddings.NewEmbedder(llm, embeddings.WithBatchSize(512))
+	log.Println("done")
+
+	log.Println("initializng embedding model...")
+	embeddingClient, err := ollama.New(
+		ollama.WithModel(embeddingModel),
+		ollama.WithServerURL(serverurl),
+		ollama.WithKeepAlive("-1m"),
+	)
 	if err != nil {
 		return nil, nil, err
 	}
 	log.Println("initialized")
-	return llm, embedder, nil
+	return llm, embeddingClient, nil
 }
