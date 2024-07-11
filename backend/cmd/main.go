2389ef8bb1e52f4a14017b6e3569f1c806028048
William Cook <web@casadecook.com>
2024-07-10 22:26:35 -0400

    Init telemetry

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"github.com/wgeorgecook/plex-recommendation/internal/pkg/telemetry"
 	"log"
 	"os"
 	"os/signal"
@@ -13,6 +15,19 @@ import (
 func main() {
 	log.Println("Hello!")
 	defer log.Println("Good bye!")
+
+	// Set up open telemetry
+	log.Println("initializing open telemetry client...")
+	shutdownOtel, err := telemetry.InitOtel(context.Background(), telemetry.WithTracer(true))
+	if err != nil {
+		panic(err)
+	}
+	log.Println("done!")
+	defer func() {
+		if err := shutdownOtel(context.Background()); err != nil {
+			log.Println("could not shutdown otel:" + err.Error())
+		}
+	}()
 
 	// Start the server in a goroutine
 	serverDone := make(chan error, 1)
